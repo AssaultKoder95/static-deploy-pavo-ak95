0939c7162a401675237f28c61544496ace589c96
Abhishek Khanna <Abhishekkhanna1995@gmail.com>
2021-05-20 15:42:05 +0530

    Updates

---
+++ precache-manifest.944d3c85c9a1db8e6f64858711e30093.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab5b0361cf782220caf2df195e915547",
+    "url": "/static-web-deploy-pavo-ak95/index.html"
+  },
+  {
+    "revision": "254dd0ed9a9155b6ea59",
+    "url": "/static-web-deploy-pavo-ak95/static/css/main.d8f5e53f.chunk.css"
+  },
+  {
+    "revision": "a1f4f216db3e2ef69a13",
+    "url": "/static-web-deploy-pavo-ak95/static/js/2.295043b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static-web-deploy-pavo-ak95/static/js/2.295043b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "254dd0ed9a9155b6ea59",
+    "url": "/static-web-deploy-pavo-ak95/static/js/main.cb1d5eab.chunk.js"
+  },
+  {
+    "revision": "9cb76b16e366a11d268d",
+    "url": "/static-web-deploy-pavo-ak95/static/js/runtime-main.29509c2f.js"
+  }
+]);
